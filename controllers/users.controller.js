655ec6051312f528090dfc1400ea3567768f84cc
alexisbarros <apenasalexis@gmail.com>
2021-02-11 21:54:27 -0300

    development: authentication endpoints

---
+++ controllers/users.controller.js
@@ -0,0 +1,265 @@
+// Modules
+var mongoose = require('mongoose');
+
+// Model
+const User = require('../models/users.model');
+
+/**
+ * Register user in db.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.create = async (req, res) => {
+
+    try {
+        
+        // Connect to database
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        // Create user in database
+        let user = await User.create({
+            email: req.body.email,
+            password: req.body.password,
+            isAdmin: req.body.isAdmin || false
+        });
+    
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        // Create user data to return
+        let userToFront = {
+            _id: user._id,
+            email: user.email,
+            isAdmin: user.isAdmin
+        };
+        
+        console.info('User created successfuly');
+        return({
+            data: userToFront,
+            message: 'User created successfuly',
+            code: 200
+        });
+
+    } catch(err) {
+
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        console.error(err.message);
+        return({
+            data: {},
+            message: err.message,
+            code: 400
+        });
+
+    }
+
+};
+
+/**
+ * Get one user by id.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.readOne = async (req, res) => {
+
+    try {
+
+        // Connect to database
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        // Get user by id
+        let user = await User.findById(req.params.id);
+        
+        // Check if user was removed
+        if(user._deletedAt) throw { message: 'User removed' };
+
+        // Create user data to return
+        let userToFront = {
+            _id: user._id,
+            email: user.email,
+            isAdmin: user.isAdmin
+        };
+        
+        // Disconnect to database
+        await mongoose.disconnect();
+        
+        console.info('User returned successfully');
+        return({
+            data: userToFront,
+            message: 'User returned successfully',
+            code: 200
+        });
+
+    } catch(err) {
+
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        console.error(err.message);
+        return({
+            data: {},
+            message: err.message,
+            code: 400
+        });
+
+    }
+
+}
+
+/**
+ * Get all users.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.readAll = async (req, res) => {
+
+    try {
+        
+        // Connect to database
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        // Get all users
+        let users = await User.find({});
+
+        // Filter user tha wasnt removed
+        let usersToFront = users.filter(user => !user._deletedAt);
+
+        // Create user data to return
+        usersToFront = usersToFront.map(user => {
+            return {
+                _id: user._id,
+                email: user.email,
+                isAdmin: user.isAdmin
+            };
+        });
+        
+        // Disconnect to database
+        await mongoose.disconnect();
+        
+        console.info('Users returned successfully');
+        return({
+            data: usersToFront,
+            message: 'Users returned successfully',
+            code: 200
+        });
+
+    } catch(err) {
+
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        console.error(err.message);
+        return({
+            data: [],
+            message: err.message,
+            code: 400
+        });
+
+    }
+
+};
+
+/**
+ * Update a user.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.update = async (req, res) => {
+
+    try {
+
+        // Connect to database
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    
+        // Update user data
+        let user = await User.findByIdAndUpdate(req.params.id, { ...req.body });
+    
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        // Create user data to return
+        let userToFront = {
+            _id: user._id,
+            email: user.email,
+            isAdmin: user.isAdmin
+        };
+        
+        console.info('User updated successfuly');
+        return({
+            data: userToFront,
+            message: 'User updated successfuly',
+            code: 200
+        });
+
+    } catch(err) {
+
+        // Disconnect to database
+        await mongoose.disconnect();
+
+        console.error(err.message);
+        return({
+            data: [],
+            message: err.message,
+            code: 400
+        });
+
+    }
+
+};
+
+/**
+ * Delete a user.
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.delete = async (req, res) => {
+
+    try {
+
+        // Connect to database
+        await mongoose.connect(process.env.DB_CONNECTION_STRING, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        // Delete user by id
+        await User.findByIdAndUpdate(req.params.id, { _deletedAt: Date.now() });
+    
+        // Disconnect to database
+        await mongoose.disconnect();
+    
+        console.info('User deleted successfuly');
+        return({
+            data: {},
+            message: 'User deleted successfuly',
+            code: 200
+        });
+        
+    } catch(err) {
+
+        // Disconnect to database
+        await mongoose.disconnect();
+        
+        console.error(err.message);
+        return({
+            data: [],
+            message: err.message,
+            code: 400
+        });
+
+    }
+
+};
---
+++ models/users.model.js
@@ -0,0 +1,96 @@
+// Modules
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+
+// Schema
+let UserSchema = new Schema({
+
+    email: {
+        type: String,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    isAdmin: {
+        type: Boolean,
+        required: false
+    },
+
+    _createdAt: { 
+        type: Date,
+        required: true,
+        default: () => {
+            if(!this._createdAt) {            
+                return Date.now();
+            }
+        },
+    },
+
+    _updatedAt: { 
+        type: Date,
+        required: true,
+        default: () => {
+            if(!this._updatedAt) {            
+                return Date.now();
+            }
+        },
+    },
+
+    _deletedAt: {
+        type: Date,
+        required: false,
+        default: null
+    },
+
+});
+
+// Encrypt password
+UserSchema.pre('save', function(next) {
+    
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+
+            next();
+
+        });
+
+    });
+
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        
+        if (err) return cb(err);
+        
+        cb(null, isMatch);
+
+    });
+
+};
+
+module.exports = mongoose.model('User', UserSchema);

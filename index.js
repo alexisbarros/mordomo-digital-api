9a58c780a19031e9c7637cf4bb28bbbe5f2c27ce
alexisbarros <apenasalexis@gmail.com>
2021-02-11 21:51:21 -0300

    development: project structure

---
+++ index.js
@@ -0,0 +1,42 @@
+// Modules
+const express = require('express');
+require('dotenv/config');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Routes modules
+const usersRoutes = require('./routes/users.route');
+const authRoutes = require('./routes/auth.route');
+const roomTypesRoutes = require('./routes/room-types.route');
+const roomsRoutes = require('./routes/rooms.route');
+
+// Middlewares
+const authMiddleware = require('./middleware/auth.middleware');
+
+// Variables
+const port = process.env.PORT || 3000;
+
+// Start express app
+const app = express();
+
+// Enable cors
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Routes
+app.use('/users', usersRoutes);
+app.use('/auth', authRoutes);
+app.use('/room-types', authMiddleware, roomTypesRoutes);
+app.use('/rooms', authMiddleware, roomsRoutes);
+
+// Run server
+app.listen(port, () => {
+    
+    console.log(`Listening on port: ${port}`);
+
+})
